55d234ed31000611b2fc1e193e746aabc32cd508
Fathi Kahin <fhkahin@gmail.com>
2024-04-06 10:04:48 +0100

    server working

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+app.get("/", (req, res) => {
+res.send("Yay Node!");
+});
 app.listen(3000, () => {
 console.log("Server is listening on port 3000. Ready to accept requests!");
 });
